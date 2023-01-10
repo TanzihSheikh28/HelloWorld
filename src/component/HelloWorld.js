7558a507baef348b145f40ad2de2da47ce82acde
tanziha sheikh <tanziha.sheikh@zehntech.com>
2023-01-10 19:40:27 +0530

    hello world code

---
+++ src/component/HelloWorld.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./HelloWorld.css"
+
+export default function HelloWorld() {
+    return (
+        <>
+            <div className="row">
+                <div className="col-12">
+                    <div className="container">
+                        <div className="content">
+                            <span>HelloWorld</span>
+                            <span>HelloWorld</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
